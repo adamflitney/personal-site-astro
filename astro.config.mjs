9563e21cd1c4d126bfe50829e3ef653af3234b78
adam.flitney <adam.flitney@yotoplay.com>
2024-01-14 20:40:56 +0000

    feat: finish the remaining tutorial features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://https://main--astro-site-purple-fox.netlify.app/"
+  site: "https://https://main--astro-site-purple-fox.netlify.app/",
+  integrations: [preact()]
 });
